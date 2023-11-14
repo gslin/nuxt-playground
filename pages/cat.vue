e67b3f5b95721476b4b56aeed3570b0596bdfc11
Gea-Suan Lin <gslin@gslin.org>
2023-11-14 22:05:42 +0800

    Implement /cat page.

--- pages/cat.vue
+++ pages/cat.vue
@@ -1,11 +1,22 @@
 <script setup>
 import { useCounterStore } from '@/stores/counter';
+
 const counter = useCounterStore();
-counter.count++;
+let data = {};
+
+const res = await fetch('https://nekos.best/api/v2/neko');
+if (res.status === 200) {
+  data = (await res.json())['results'][0];
+}
 </script>
 
 <template>
 <h3>cat</h3>
 <hr/>
 <p>Current count: {{ counter.count }}.</p>
+<hr/>
+<dd>
+<dl>artist_name + artist_href</dl>
+<dt><a :href="data.artist_href">{{ data.artist_name }}</a></dt>
+</dd>
 </template>
