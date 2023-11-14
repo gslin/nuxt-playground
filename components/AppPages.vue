c1dd014e205a3828e692182be322f82fcf8913d4
Gea-Suan Lin <gslin@gslin.org>
2023-11-14 21:43:45 +0800

    Move page information to components/.

---
+++ components/AppPages.vue
@@ -0,0 +1,8 @@
+<template>
+<h2>Pages</h2>
+<ul>
+  <li><NuxtLink to="/">Index</NuxtLink></li>
+  <li><NuxtLink to="/about">About</NuxtLink></li>
+  <li><NuxtLink to="/cat">Cat</NuxtLink></li>
+</ul>
+</template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -3,11 +3,7 @@
 <AppHeader/>
 <div class="flex flex-row my-4">
   <div class="shrink mr-4">
-    <h2>Pages</h2>
-    <ul>
-      <li><NuxtLink to="/">Index</NuxtLink></li>
-      <li><NuxtLink to="/about">About</NuxtLink></li>
-    </ul>
+    <AppPages/>
   </div>
   <div class="ml-4">
     <NuxtPage/>
