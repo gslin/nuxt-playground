8f1ede2950f25a70cea15a0489ad55ae6608b06d
Gea-Suan Lin <gslin@gslin.org>
2023-11-14 21:44:22 +0800

    Add /cat.

---
+++ pages/cat.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { useCounterStore } from '@/stores/counter';
+const counter = useCounterStore();
+counter.count++;
+</script>
+
+<template>
+<h3>cat</h3>
+<hr/>
+<p>Current count: {{ counter.count }}.</p>
+</template>
