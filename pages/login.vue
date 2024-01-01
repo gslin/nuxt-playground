7adbd4b3aa2d32248a5c55e75fcccc2567724d7f
Gea-Suan Lin <gslin@gslin.org>
2024-01-01 11:17:03 +0800

    Fix from eslint's messages.

--- pages/login.vue
+++ pages/login.vue
@@ -1,8 +1,8 @@
 <script setup>
-const d = reactive({username: '', password: ''});
+const d = reactive({username: '', password: ''})
 
-const r = ref({});
-const c = ref(0);
+const r = ref({})
+const c = ref(0)
 
 async function login() {
   const res = await fetch('https://home.gslin.org/tmp/test-login.php', {
@@ -11,23 +11,41 @@ async function login() {
     headers: {
       'Content-Type': 'application/json',
     },
-    body: JSON.stringify(d),
+    body: JSON.stringify(d)
   });
 
-  r.value = await res.json();
-  c.value = res.status;
+  r.value = await res.json()
+  c.value = res.status
 }
 </script>
 
 <template>
-<h3>Login</h3>
-<hr/>
-<form @submit.prevent="login();">
-  <input class="border" name="username" placeholder="username" type="text" v-model="d.username" />
-  <input class="border" name="password" placeholder="password" type="password" v-model="d.password" />
-  <input class="border" name="submit" type="submit"/>
-</form>
-<p>Username: <code>{{ d.username }}</code>.</p>
-<p>Code: <code>{{ c }}</code>.</p>
-<p>Response: <code>{{ r }}</code>.</p>
+  <div>
+    <h3>Login</h3>
+    <hr>
+    <form @submit.prevent="login();">
+      <input
+        v-model="d.username"
+        class="border"
+        name="username"
+        placeholder="username"
+        type="text"
+      >
+      <input
+        v-model="d.password"
+        class="border"
+        name="password"
+        placeholder="password"
+        type="password"
+      >
+      <input
+        class="border"
+        name="submit"
+        type="submit"
+      >
+    </form>
+    <p>Username: <code>{{ d.username }}</code>.</p>
+    <p>Code: <code>{{ c }}</code>.</p>
+    <p>Response: <code>{{ r }}</code>.</p>
+  </div>
 </template>
